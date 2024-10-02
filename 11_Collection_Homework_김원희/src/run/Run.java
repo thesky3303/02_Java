6a7e7c413f232d070fcc3343153c713b56dcc8af
thesky3303 <thesky3303@gmail.com>
2024-10-02 15:53:11 +0900

    1002

--- 11_Collection_Homework_김원희/src/run/Run.java
+++ 11_Collection_Homework_김원희/src/run/Run.java
@@ -1,15 +1,9 @@
 package run;
 
-import study.Monkey;
-import study.Tiger;
-import study.Zoo;
+import service.Service;
 
-public class Run {
-	public static void main(String[] args) {
-		Zoo zoo = new Zoo();
-		
-		zoo.addAnimal(new Tiger("호랑이"));
-		zoo.addAnimal(new Monkey("원숭이"));
-		zoo.displayMenu();
-	}
+public class Run2 {
+public static void main(String[] args) {
+	new Service().displaymenu();
+}
 }
--- 11_Collection_Homework_김원희/src/run/Run2.java
+++
@@ -1,9 +0,0 @@
-package run;
-
-import service.Service;
-
-public class Run2 {
-public static void main(String[] args) {
-	new Service().displaymenu();
-}
-}
