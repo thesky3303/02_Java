e908edce594ecd0b8149838c9a8a0c587a1601e6
thesky3303 <thesky3303@gmail.com>
2024-10-04 15:09:41 +0900

    1004편집

--- 00_JavaBasic/src/test/Run.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Run {
-	public static void main(String[] args) {
-		Service service = new Service();
-		service.displayMenu();
-	}
-}
--- 11_Collection_Homework_김원희/src/run/Run.java
+++
@@ -1,9 +0,0 @@
-package run;
-
-import service.Service;
-
-public class Run {
-public static void main(String[] args) {
-	new Service().displaymenu();
-}
-}
